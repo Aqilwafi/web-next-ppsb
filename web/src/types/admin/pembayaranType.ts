e6b7ccaa85a34eee270154511de2552a808d7ee1
Aqil Wafi <m.aqil.wafi@gmail.com>
2025-11-18 20:03:37 +0700

    hapus admin sementara

--- web/src/types/admin/pembayaranType.ts
+++
@@ -1,8 +0,0 @@
-export type Pembayaran = {
-  id: string;
-  nama_lengkap: string;
-  lembaga: string;
-  kelas: string;
-  status: "Lunas" | "Belum Lunas";
-  bukti?: string | null;
-};
--- web/src/types/admin/userProgressType.ts
+++
@@ -1,9 +0,0 @@
-export type UserProgress = {
-  id: string;
-  nama_lengkap: string;
-  lembaga: string;
-  tingkatan: string;
-  email: string;
-  completedSteps: number;
-  completedStepLabels: string; // 0-6
-};
